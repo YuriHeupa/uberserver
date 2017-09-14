02b23f53f813318fa5d277f03d3f21874cdb277a
Yuri Heupa <yurihernaniheupa@gmail.com>
2017-09-14 17:35:27 -0300

    Initial setup

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 # Installation
 ```
 # git clone git@github.com:spring/uberserver.git
-# virtualenv ~/virtenvs/uberserver
+# python3 -m venv ~/var/www/.virtualenvs/uberserver
 # source ~/virtenvs/uberserver/bin/activate
 # pip install SQLAlchemy pycrypto twisted pyOpenSSL GeoIP mysqlclient
 ```
@@ -16,10 +16,35 @@ Without further configuration this will create a SQLite database (server.db).
 Performance will be OK for testing and small setups. For production use,
 setup MySQL/PostgreSQL/etc.
 
+
+
+## Installing and configuring GeoIP
+
+
+1. First create the GeoIP directory
+
+        sudo mkdir /usr/local/share/GeoIP
+
+2. Now, move the GeoLite City binary database to it's new location.
+
+        sudo mv geoip/GeoLiteCity.dat /usr/local/share/GeoIP/
+
+3. Install the GeoIP C Library
+
+        cd geoip/GeoIP-1.4.8
+
+4. Now, configure and install the GeoIP C library by issuing the following commands.
+
+        ./configure
+        make
+        make check
+        sudo make install
+
+
 # Usage
 ```
-# source ~/virtenvs/uberserver/bin/activate
-# ./server.py
+# source ~/var/www/.virtualenvs/uberserver/bin/activate
+# ./server.py -v 103.0
 ```
 
 # Logs
---
+++ fabfile.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Version 1.0
+
+import time
+import os
+
+from fabric.api import *
+
+##
+## available environments
+##
+
+def amazon():
+    env.hosts = ['centos@18.231.48.193']
+    env.app_root = '/srv/uberserver/'
+    env.server_script = './server.py'
+    env.git_origin = 'git@github.com:YuriHeupa/uberserver.git'
+    env.key_filename = "lohann.pem"
+    env.password = "Lpcf97109011"
+    env.git_branch = 'master'
+    env.virtual = '/var/www/.virtualenvs/uberserver/bin/activate'
+
+##
+## available commands
+##
+
+def deploy():
+    start = time.time()
+
+    ## validate environment
+    if not hasattr(env, 'app_root'):
+        print('ERROR: unknown environment.')
+        os.sys.exit(1)
+
+    ## clone repository
+    command = 'test -d %s.git || git clone %s %s -b %s'
+    sudo(command % (env.app_root, env.git_origin, env.app_root, env.git_branch))
+
+    ## update repository
+    command = 'cd "%s" && git reset --hard && git pull && git checkout -B %s origin/%s && git pull'
+    run(command % (env.app_root, env.git_branch, env.git_branch))
+
+    ## update python packages
+    command = 'source %s; cd %s; pip install -r requirements.txt' % (env.virtual, env.app_root)
+    run(command)
+    # sudo(command, user='www-data')
+
+    ## restart service
+    command = 'cd "%s" && %s'
+    run(command % (env.app_root, env.server_script))
+
+    final = time.time()
+    puts('execution finished in %.2fs' % (final - start))
